71a17be6c0c6f72694fdf80df6a943af8344bd3e
Taima-Safi <TAIMASAFI2000@gmail.com>
2024-01-29 20:57:45 +0300

    .gitignore

---
+++ Medical-Api/Controllers/SpicialitiesController.cs
@@ -0,0 +1,24 @@
+﻿using M_Services.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Medical.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpicialitiesController : ControllerBase
+    {
+        private readonly ISpecialitiesService _servicesService;
+
+        public SpicialitiesController(ISpecialitiesService servicesService)
+        {
+            _servicesService = servicesService;
+        }
+
+        //[HttpPost("AddService")]
+        //public async Task<IActionResult> AddService()
+        //{
+        //    return Ok(await _servicesService.AddService(dto));
+        //}
+    }
+}
