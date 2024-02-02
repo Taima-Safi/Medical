2cbe5e0a89d90b5eb4c97c04aa032026ed592158
Taima-Safi <TAIMASAFI2000@gmail.com>
2024-02-02 17:11:24 +0300

    Client Controller endpoints

---
+++ Medical-Api/Controllers/ClientsController.cs
@@ -0,0 +1,47 @@
+﻿using M_Services.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Medical.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly ISpecialitiesService _specialitiesService;
+        private readonly IServicesService _servicesService;
+        private readonly IDoctorsService _doctorsService;
+
+        public ClientsController(ISpecialitiesService specialitiesService, IServicesService servicesService, IDoctorsService doctorsService)
+        {
+            _specialitiesService = specialitiesService;
+            _servicesService = servicesService;
+            _doctorsService = doctorsService;
+        }
+
+        //speciality Endpoints
+
+        [HttpGet("GetSpecialities")]
+        public async Task<IActionResult> GetSpecialities()
+        {
+            return Ok(await _specialitiesService.GetAllSpecialities());
+        }
+        [HttpGet("GetSpecialitiesCount")]
+        public async Task<IActionResult> GetSpecialitiesCount()
+        {
+            return Ok(await _specialitiesService.GetSpecialitiesCount());
+        }
+
+        //Service Endpoints
+
+
+
+
+        //Doctor Endpoints
+        [HttpGet("DoctorsNumberForSpeciality")]
+        public async Task<IActionResult> DoctorsForSpeciality(int specialityId)
+        {
+            return Ok(await _doctorsService.DoctorsForSpeciality(specialityId));
+        }
+    }
+}
